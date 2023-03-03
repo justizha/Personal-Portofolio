03273450dbd7812226f450b2f07aa80e46301d3e
Alfath Izha Barikallah <alfathizhaaaaaaa@gmail.com>
2023-03-03 23:28:47 +0700

    three?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,48 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["index.html"],
+  safelist: {
+    standard: [
+        'text-2xl',
+        'text-3xl',
+        'text-4xl',
+        'text-5xl',
+        'text-6xl',
+        'sm:text-2xl',
+        'sm:text-3xl',
+        'sm:text-4xl',
+        'sm:text-5xl',
+        'sm:text-6xl',
+        'md:text-2xl',
+        'md:text-3xl',
+        'md:text-4xl',
+        'md:text-5xl',
+        'md:text-6xl',
+        'lg:text-2xl',
+        'lg:text-3xl',
+        'lg:text-4xl',
+        'lg:text-5xl',
+        'lg:text-6xl',
+    ]
+    },
   darkMode: "class",
   theme: {
     container: {
       center: true,
       padding: "26px",
     },
-    
+    fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+      'lg': '1.125rem',
+      'xl': '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
     extend: {
       colors: {
         primary: "#0d9488",
